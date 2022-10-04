eeb0b97127aaef3a43be5a7a531c89a0232c061a
Alexey Skoblikov <happycow32@yandex.ru>
2022-10-04 21:41:22 +0300

    Task 1

---
+++ task1/task1.py
@@ -0,0 +1,21 @@
+# Вычислить число c заданной точностью d
+
+# Пример:
+
+# - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
+
+import math
+
+
+def calc_factor(num):
+    result = 0
+    while num % 1 > 0:
+        num *= 10
+        result += 1
+    return result
+
+
+fract_accur = float(input("Введите необходимую точноть: "))
+rounded_pi = round(math.pi, calc_factor(fract_accur))
+print(rounded_pi)
+
