2ba29a7f5be0f873bd42ea4bae0e72fdb78723c7
Alexey Skoblikov <happycow32@yandex.ru>
2022-10-06 22:35:23 +0300

    Task 3 final

--- task3/task3.py
+++ task3/task3.py
@@ -12,3 +12,18 @@ def unique_num(list):
 num_list = [1, 2, 3, 3, 4, 5, 5, 8, 3, 1, 2]
 new_list = unique_num(num_list)
 print(new_list)
+
+#=================== пример выполнения с семинара через словарь и counter
+
+# from collections import Counter
+
+# nums = input('Введите числа через пробел: ').split()
+# counts = Counter(nums)
+# print([i for i in counts if counts[i] == 1])
+
+
+# res = []
+# for key in counts:
+#     if counts[key] == 1:
+#         res.append(key)
+# print(res)
